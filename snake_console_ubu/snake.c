5e9bb4f1505345815622f3cab68d917c0e15747a
WatchingBoss <opil94@yandex.ru>
2018-04-14 20:36:17 +0300

    Snake console ubuntu

---
+++ snake_console_ubu/snake.c
@@ -0,0 +1,196 @@
+#include <ncurses.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+void game();
+void action(int, int);
+void control();
+void logic();
+void start();
+
+bool gameOver;
+const int width = 30, height = 30;
+int x, y, fX, fY, ch;
+int tail = 0;
+int tailX[50], tailY[50];
+int score = 0;
+
+enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN} ctrl;
+
+int main()
+{
+	srand(time(NULL));
+	
+	game();
+
+    return 0;
+}
+
+void
+game()
+{
+	char answer = 'y';
+
+    initscr();
+	raw();
+	noecho();
+	curs_set(FALSE); 
+	keypad(stdscr, TRUE);
+
+	const int startx = (COLS - width) / 2, starty = (LINES - height) / 2;
+
+
+	while(answer == 'y' || answer == 'Y')
+	{
+		nodelay(stdscr, TRUE);
+		start();
+		
+		while(!gameOver)
+		{ 
+			action(starty, startx);
+			control();
+			logic();
+		}
+
+		if(ch != 'x')
+		{
+			nodelay(stdscr, FALSE);
+			mvprintw(starty - 5, startx, "Do you want play again? (y/n)");
+			refresh();
+			answer = getch();
+			tail = 0;
+			score = 0;
+			mvprintw(starty - 5, startx, "                              ");
+			mvprintw(starty - 2, startx + 10, "             ");
+		}
+		else
+			answer = 'x';
+	} 
+
+	sleep(1);
+
+	curs_set(TRUE);
+	endwin();
+}
+
+void
+action(int starty, int startx)
+{
+	WINDOW *win;
+	win = newwin(height, width, starty, startx);
+	box(win, 0, 0);
+	wborder(win, '*', '*', '*', '*', '+', '+', '+', '+');
+
+	mvwprintw(win, fY, fX, "%c", 'U');
+	mvwprintw(win, y, x, "%c", 'O');
+	for(int i = 0; i < tail; ++i)
+	{
+		mvwprintw(win, tailY[i], tailX[i], "%c", 'o');
+	}
+
+	wrefresh(win);
+
+	mvprintw(starty - 2, startx + 10, "Score: %d", score);
+
+	refresh();
+	usleep(80000);
+}
+
+void
+control()
+{
+	if((ch = getch()) != ERR)
+	{
+		switch(ch)
+		{
+			case 'a':
+			case KEY_LEFT:
+				ctrl = LEFT;
+				break;
+			case 'w':
+			case KEY_UP:
+				ctrl = UP;			
+				break;
+			case 'd':
+			case KEY_RIGHT:
+				ctrl = RIGHT;			
+				break;
+			case 's':
+			case KEY_DOWN:
+				ctrl = DOWN;			
+				break;
+			case 'x':
+				gameOver = true;
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+void
+logic()
+{
+	int prevX = tailX[0], prevY = tailY[0],
+		prev2X, prev2Y;
+	tailX[0] = x;
+	tailY[0] = y;
+	for(int i = 1; i < tail; ++i)
+	{
+		prev2X   = tailX[i];
+		prev2Y   = tailY[i];
+		tailX[i] = prevX;
+		tailY[i] = prevY;
+		prevX    = prev2X;
+		prevY    = prev2Y;
+	}
+	
+	switch(ctrl)
+	{
+		case LEFT:
+			--x;
+			break;
+		case UP:
+			--y;
+			break;
+		case RIGHT:
+			++x;
+			break;
+		case DOWN:
+			++y;
+			break;
+		default:
+			break;
+	}
+
+	if(x == 0) x = width - 1; else if(x == width - 1) x = 1;
+	if(y == 0) y = height -1; else if(y == height - 1) y = 1;
+
+	if(x == fX && y == fY)
+	{
+		fX = (rand() % (width - 2)) + 1;
+		fY = (rand() % (height - 2)) + 1;
+		++score;
+		++tail;
+	}
+
+	for(int i = 0; i < tail; ++i)
+	{
+		if(x == tailX[i] && y == tailY[i])
+			gameOver = true;
+	}
+}
+
+void
+start()
+{
+	gameOver = false;
+	
+	x = width / 3;
+	y = height / 3;
+	fX = (rand() % (width - 2)) + 1;
+	fY = (rand() % (height - 2)) + 1;
+}
+
+
