876c3e9896616643b9ed3681529d62ba90b31a14
Watchingboss <opil94@yandex.ru>
2018-05-03 18:39:55 +0300

    Simple game. Need configure moving boolets

--- shoot_console_ubu/shoot.c
+++ shoot_console_ubu/shoot.c
@@ -1,179 +1,207 @@
 #include <ncurses.h>
-#include <stdlib.h>  /* We need rand() */
-#include <time.h>    /* We need time() for actual random */
-#include <unistd.h>  /* We need usleep() */
+#include <stdlib.h> // we use rand() & srand()
+#include <time.h>   // we need actual random()
+#include <unistd.h> // we use usleep()
 
-typedef struct WIN_PARAM
-{
-	int positionY, positionX;
-	int width, height;
-} WinMain;
+#define WIN_WIDTH 25
+#define WIN_HEIGHT 30
+#define WIN_Y 6
+#define WIN_X ((COLS - WIN_WIDTH) / 2)
+#define AMOUNT_BOOLET 15
+#define AMOUNT_TARGET 20
+#define MISS_LIMIT 5
 
-typedef struct PLAYER_PARAM
+typedef struct GAME_UNIT
 {
+	float pY, pX;
 	char skin;
-	int positionY, positionX;
-} Player;
+} GameUnit;
 
-void init_all(WinMain *, Player *, Player *, int *);
-void drawWindow(WinMain *);
-void action(WinMain *, Player *, Player *, int *);
+enum controlDefinition {STOP, LEFT, RIGHT} ctrl;
+bool shooting;
+bool gameOver;
+int score;
+int keyInput;
+int numBoolet;
+int lose;
+
+void shoot();
+void graphic(WINDOW *, GameUnit *, GameUnit *, GameUnit *);
 void input();
-void logic(WinMain *, Player *, Player *, int *);
-void start();
-
-enum control {STOP, LEFT, RIGHT, SPACE} ctrl;
+void logic(WINDOW *win, GameUnit *, GameUnit *, GameUnit *);
+void start(GameUnit *, GameUnit *, GameUnit *);
 
 int main()
 {
-	WinMain win;       /* Our window   */
-	Player pl1;	       /* Player 1     */
-	int arr = 10;
-	Player sh[arr];         /* Boolet shots */
-
-	char choose = 'y'; /* Play again?*/
-	int inputCh;       /* Input from keyboard */
-	int score = 0;
-	bool gameOver;
-	
+	srand(time(NULL));
 	initscr();
-	curs_set(0);
 	start_color();
-	raw();
 	keypad(stdscr, TRUE);
+	cbreak();
 	noecho();
-	init_pair(1, COLOR_CYAN, COLOR_YELLOW);
-	init_pair(2, COLOR_RED, COLOR_MAGENTA);
-	init_pair(3, COLOR_BLUE, COLOR_WHITE);
+	curs_set(FALSE);
 
-	srand(time(NULL)); /* Make actual random for rand() */
+	shoot();
 
-	init_all(&win, &pl1, sh, &arr);
+	curs_set(TRUE);
+	endwin();
+	return(0);
+}
 
-    while(choose == 'y' || choose == 'Y')
+void shoot()
+{
+	WINDOW *win;
+	char goGame = 'y';
+
+	GameUnit player;
+	player.pY   = WIN_HEIGHT - 2;
+	player.pX   = WIN_WIDTH / 2;
+	player.skin = '^';
+
+	GameUnit boolet[AMOUNT_BOOLET];
+	for(int i = 0; i < AMOUNT_BOOLET; ++i)
 	{
-		nodelay(stdscr, TRUE);
-		start(&gameOver);
+		boolet[i].pY =  player.pY - 1;
+		boolet[i].pX =  player.pX;
+		boolet[i].skin =  ' ';
+	} // end forn
+
+	GameUnit target[AMOUNT_TARGET];
+	for(int i = 0; i < AMOUNT_TARGET; ++i)
+	{
+		target[i].pY = rand() % (AMOUNT_TARGET * 5) - (AMOUNT_TARGET * 5);
+		target[i].pX = rand() % (WIN_WIDTH - 2) + 2;
+		target[i].skin = rand() % (91 - 65) + 65; // range A - Z
+	} // end for
+
+	win = newwin(WIN_HEIGHT, WIN_WIDTH, WIN_Y, WIN_X);
+
+	while(goGame == 'y' || goGame == 'Y')
+	{
+		start(&player, boolet, target);
 		while(!gameOver)
 		{
-			drawWindow(&win);
-			action(&win, &pl1, sh, &arr);
-			input(&gameOver, &inputCh);
-			logic(&win, &pl1, sh, &arr);
-			usleep(50000);
+			input();
+			logic(win, &player, boolet, target);
+			graphic(win, &player, boolet, target);
+			usleep(100000);
 		} // end while
-		
-		if(inputCh != 'q')
+		if(keyInput != 'q')
 		{
-			mvprintw(1, 5, "May be although one game? Press: y");
+			nodelay(stdscr, FALSE);
+			if(score == AMOUNT_TARGET)
+				mvprintw(1, COLS / 2.5, "You are good! You win:)");
+			else
+				mvprintw(1, COLS / 2.5, "You are loser LMAO ^__^");
+			mvprintw(3, COLS / 2.5, "Maybe play again? (y/n)");
 			refresh();
-			choose = getch();
-			score = 0;
-			pl1.positionX = COLS / 2;
+			goGame = 'P';
+			while(goGame != 'y' && goGame != 'Y' && goGame != 'n' && goGame != 'N')
+				goGame = getch();
 		} // end if
 		else
-			choose = 'n';
+			goGame = 'n';   
 	} // end while
 
-	curs_set(1);
-	endwin();
-
-    return(0);
+//	sleep(1);
 }
 
-void init_all(WinMain *pWin, Player *pPl1, Player *pSh, int *arr)
+void graphic(WINDOW *win, GameUnit *player, GameUnit *boolet, GameUnit *target)
 {
-	pWin->width     = 50;
-	pWin->height    = 50;
-	pWin->positionY = 5;
-	pWin->positionX = (COLS - pWin->width) / 2;
+	werase(win);
+	
+	box(win, 0, 0);
 
-	pPl1->positionY = pWin->height - 2;
-	pPl1->positionX = COLS / 2;
-	pPl1->skin = '^';
+	mvwaddch(win, player->pY, player->pX, player->skin);
+	
+	for(int i = 0; i < AMOUNT_BOOLET; ++i)
+		mvwaddch(win, boolet[i].pY, boolet[i].pX, boolet[i].skin);
 
-	for(int i = 0; i < *arr; ++i)
-	{
-		pSh[i].skin = ' ';
-		pSh[i].positionY = pPl1->positionY - 1;
-		pSh[i].positionX = pPl1->positionX;
-	}
-}
+	for(int i = 0; i < AMOUNT_TARGET; ++i)
+		if(target[i].pY > 0)
+			mvwaddch(win, target[i].pY, target[i].pX, target[i].skin);
 
-void drawWindow(WinMain *pWin)
-{
-	int y, x, w, h;
-	char board = '|';
-	w = pWin->width;
-	h = pWin->height;
-	y = pWin->positionY;
-	x = pWin->positionX;
+	mvprintw(5, COLS / 2.5, "Score: %d", score);
+	mvprintw(WIN_Y + 5, 1, "You lost: %d", lose);
 
-	attron(COLOR_PAIR(1));
-	mvvline(y, x, board, h);
-	mvvline(y, x + w, board, h);
-	attroff(COLOR_PAIR(1));
-}
-
-void action(WinMain *pWin, Player *pPl1, Player *pSh, int *arr)
-{
-	int y, x, shotY, shotX;
-	char skin, shotSkin;
-	y        = pPl1->positionY;
-	x        = pPl1->positionX;
-	skin     = pPl1->skin;
-	shotY    = pSh->positionY;
-	shotX    = pSh->positionX;
-    shotSkin = pSh->skin;
-
-	for(int i = pWin->positionX + 1; i < pWin->positionX + pWin->width; i++)
-	{
-		if(i == x)
-			mvprintw(y, x, "%c", skin);
-		else
-			mvprintw(y, i, " ");
-	}
-
-	for(int i = 0; i < *arr; ++i)
-	{
-		mvprintw(shotY, shotX, "%c", shotSkin);
-	}
 	refresh();
+	wrefresh(win);
 }
 
-void logic(WinMain *pWin, Player *pPl1, Player *pSh, int *arr)
+void logic(WINDOW *win, GameUnit *player, GameUnit *boolet, GameUnit *target)
 {
-	int i = 0;
 	switch(ctrl)
 	{
 		case LEFT:
-			--pPl1->positionX;
-			--pSh->positionX;
+			--player->pX;
+			for(int i = 0; i < AMOUNT_BOOLET; ++i)
+			{
+				if(boolet[i].pY == player->pY - 1)
+					--boolet[i].pX;
+			} // end for
 			break;
 		case RIGHT:
-			++pPl1->positionX;
-			++pSh->positionX;
-			break;
-		case SPACE:
-			pSh[i].skin = '|';
-			++i;
+			++player->pX;
+			for(int i = 0; i < AMOUNT_BOOLET; ++i)
+			{
+				if(boolet[i].pY == player->pY - 1)
+					++boolet[i].pX;
+			} // end for
 			break;
 		default:
 			break;
-			
 	} // end switch
 
-	if(pPl1->positionX == pWin->positionX)
-		pPl1->positionX = pWin->positionX + pWin->width - 1;
-	if(pPl1->positionX == pWin->positionX + pWin->width)
-		pPl1->positionX = pWin->positionX + 1;
+	if(shooting)
+	{
+		++numBoolet;
+		boolet[numBoolet].skin = '|';
+		shooting = false;
+	} // end if
+
+	if(player->pX == 0) player->pX = WIN_WIDTH - 2;
+	else if(player->pX == WIN_WIDTH - 1) player->pX = 1;
+
+	if(numBoolet >= 0)
+	{
+		for(int i = 0; i <= numBoolet; ++i)
+		{
+			if(boolet[i].pY != 1)
+				--boolet[i].pY;
+			else
+			{
+				boolet[i].pY = player->pY - 1;
+				boolet[i].pX = player->pX;
+				boolet[i].skin = ' ';
+			} // end else
+		} // end for
+	} // end if
+	if(numBoolet == AMOUNT_BOOLET - 1) numBoolet = 0;
+
+	for(int i = 0; i < AMOUNT_TARGET; ++i)
+	{
+		for(int j = 0; j <= numBoolet; ++j)
+		{
+			if(target[i].pY == boolet[j].pY && target[i].pX == boolet[j].pX)
+			{
+				target[i].pY = -(AMOUNT_TARGET * 5);
+				++score;
+			} // end if
+		} // end for
+		if(target[i].pY == WIN_HEIGHT)
+			++lose;
+		target[i].pY += 0.25;
+	} // end for
+
+	if(score == AMOUNT_TARGET - MISS_LIMIT) gameOver = true;
+//	if(lose == MISS_LIMIT) gameOver = true;
 }
 
-void input(bool *gameOver, int *inputCh)
+void input()
 {
-	if((*inputCh = getch()) != ERR)
+	if((keyInput = getch()) != ERR)
 	{
-		switch(*inputCh)
+		switch(keyInput)
 		{
 			case KEY_LEFT:
 				ctrl = LEFT;
@@ -182,19 +210,46 @@ void input(bool *gameOver, int *inputCh)
 				ctrl = RIGHT;
 				break;
 			case ' ':
-				ctrl = SPACE;
+				shooting = true;
 				break;
 			case 'q':
-				*gameOver = true;
+				gameOver = true;
 				break;
 			default:
-				break;					 
+				break;
 		} // end switch
 	} // end if
 }
 
-void start(bool *gameOver)
+void start(GameUnit *player, GameUnit *boolet, GameUnit *target)
 {
-	*gameOver = false;
+	clear();
+	gameOver = false;
 	ctrl = STOP;
+	score = 0;
+	numBoolet = -1;
+	shooting = false;
+	nodelay(stdscr, TRUE);
+
+	mvprintw(WIN_Y + 1, 1, "Goal: %d", (AMOUNT_TARGET - MISS_LIMIT));
+	mvprintw(WIN_Y + 2, 1, "Whole targets are: %d", AMOUNT_TARGET);
+	mvprintw(WIN_Y + 3, 1, "You will lose if miss %d targets", MISS_LIMIT);
+
+
+	player->pY   = WIN_HEIGHT - 2;
+	player->pX   = WIN_WIDTH / 2;
+
+	for(int i = 0; i < AMOUNT_BOOLET; ++i)
+	{
+		boolet[i].pY =  player->pY - 1;
+		boolet[i].pX =  player->pX;
+		boolet[i].skin =  ' ';
+	}
+
+	for(int i = 0; i < AMOUNT_TARGET; ++i)
+	{
+		target[i].pY = rand() % (AMOUNT_TARGET * 5) - (AMOUNT_TARGET * 5);
+		target[i].pX = rand() % (WIN_WIDTH - 2) + 2;
+		target[i].skin = rand() % (91 - 65) + 65; // range A - Z
+	}
 }
